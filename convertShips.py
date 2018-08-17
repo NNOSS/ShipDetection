1fa5d5c76e2ae6df17dc9f9f1886ffa6bdfdbf08
Nick Steelman <gns126@gmail.com>
2018-08-17 12:04:15 -0400

    working coarse labels

--- convertShips.py
+++ convertShips.py
@@ -7,9 +7,10 @@ import matplotlib.image as mpimg
 FILEPATH = '/Data/ShipDetection/'
 CSV_FILENAME = FILEPATH + 'train_ship_segmentations.csv'
 DATA_FILEPATH = FILEPATH + 'train/'
-TRAIN_LABEL_SAVE = FILEPATH + 'train_labels_6'
-TRAIN_INPUT_SAVE = FILEPATH + 'train_images_6'
-DIVIDEND = 4
+TRAIN_LABEL_SAVE = FILEPATH + 'train_labels'
+TRAIN_INPUT_SAVE = FILEPATH + 'train_images'
+TRAIN_INPUT_SAVE_COARSE = FILEPATH + 'train_images_coarse'
+DIVIDEND = 12
 WIDTH = 768
 HEIGHT = 768
 NEW_HEIGHT = HEIGHT / DIVIDEND
@@ -24,29 +25,55 @@ files_dict_gen = loadCSV.load_csv(CSV_FILENAME,batch_size)
 file_dict = next(files_dict_gen, None)
 count = 0
 while file_dict:
-    # input_group_div = np.zeros((len(file_dict)*DIVIDEND*DIVIDEND,NEW_WIDTH,NEW_HEIGHT,3),dtype=np.uint8)
-    # label_group_div = np.full((len(file_dict)*DIVIDEND*DIVIDEND,NEW_WIDTH,NEW_HEIGHT),.1,dtype=np.float32)
-    input_group_div = np.zeros((len(file_dict),WIDTH,HEIGHT,3),dtype=np.uint8)
-    label_group_div = np.full((len(file_dict),DIVIDEND,DIVIDEND),.1,dtype=np.float32)
+    # input_group = np.zeros((len(file_dict)*DIVIDEND*DIVIDEND,NEW_WIDTH,NEW_HEIGHT,3),dtype=np.uint8)
+    # label_group = np.full((len(file_dict)*DIVIDEND*DIVIDEND,NEW_WIDTH,NEW_HEIGHT),.1,dtype=np.float32)
+    input_group = np.zeros((len(file_dict),WIDTH,HEIGHT,3),dtype=np.uint8)
+    coarse_label = np.zeros((len(file_dict),DIVIDEND,DIVIDEND),dtype=np.uint8)
+    fine_label = np.zeros((len(file_dict),WIDTH,HEIGHT),dtype=np.uint8)
+
     for i, temp in enumerate(file_dict.items()):
         k,v = temp
-        try:
-            image = Image.open(DATA_FILEPATH + k)
-            np_image = np.array(image,dtype=np.uint8)
-            for j in range(DIVIDEND*DIVIDEND):
-                x_a = (j // DIVIDEND) * NEW_WIDTH
-                y_a = (j % DIVIDEND) * NEW_HEIGHT
-                label_group[i*DIVIDEND*DIVIDEND + j] = v[x_a:x_a + NEW_WIDTH, y_a:y_a + NEW_HEIGHT]
-                input_group[i*DIVIDEND*DIVIDEND + j] = np_image[x_a:x_a + NEW_WIDTH, y_a:y_a + NEW_HEIGHT]
-            count+= 1
-            if not count%1000:
-                print('Count: ' + str(count))
-        except:
-            continue
+        # try:
+        image = Image.open(DATA_FILEPATH + k)
+        np_image = np.array(image,dtype=np.uint8)
+        # input_group[i] = np_image
+        # fine_label[i] = v
+        for j in range(DIVIDEND*DIVIDEND):
+            # x_a = (j // DIVIDEND) * NEW_WIDTH
+            # y_a = (j % DIVIDEND) * NEW_HEIGHT
+            # label_group[i*DIVIDEND*DIVIDEND + j] = v[x_a:x_a + NEW_WIDTH, y_a:y_a + NEW_HEIGHT]
+            # input_group[i*DIVIDEND*DIVIDEND + j] = np_image[x_a:x_a + NEW_WIDTH, y_a:y_a + NEW_HEIGHT]
+            x_a = (j // DIVIDEND)
+            y_a = (j % DIVIDEND)
+            has_boat = np.amax(v[x_a * NEW_WIDTH:x_a * NEW_WIDTH + NEW_WIDTH, y_a * NEW_HEIGHT:y_a * NEW_HEIGHT + NEW_HEIGHT])
+            if has_boat and x_a > 0 and y_a > 0:
+                im = Image.fromarray(np_image[(x_a-1) * NEW_WIDTH:(x_a+1) * NEW_WIDTH + NEW_WIDTH, (y_a-1) * NEW_HEIGHT:(y_a+1) * NEW_HEIGHT + NEW_HEIGHT])
+                im.show()
+                raw_input(" ")
+
+            coarse_label[i, x_a, y_a] = has_boat
+            # if has_boat:
+            #     print('HAS BOAT: ' + str(has_boat))
+            #     print('X: ' + str(x_a))
+            #     print('Y: ' + str(y_a))
+            #     image.show()
+            #     im = Image.fromarray(coarse_label[i]* 254)
+            #     im = im.resize((300,300))
+            #     im.show()
+            #     raw_input(" ")
+
+
+        count+= 1
+        if not count%100:
+            print('Count: ' + str(count))
+        # except:
+        #     continue
 
     # print(input_group)
     # print(label_group)
-    append_binary_file(TRAIN_INPUT_SAVE,input_group.tobytes())
-    append_binary_file(TRAIN_LABEL_SAVE,label_group.tobytes())
+    # append_binary_file(TRAIN_INPUT_SAVE,input_group.tobytes())
+    # append_binary_file(TRAIN_LABEL_SAVE,fine_label.tobytes())
+    exit()
+    append_binary_file(TRAIN_INPUT_SAVE_COARSE,coarse_label.tobytes())
     file_dict = next(files_dict_gen, None)
 print('DOOOOOOOOONE')
--- model.py
+++ model.py
@@ -9,36 +9,43 @@ import numpy as np
 FILEPATH = '/Data/ShipDetection/'
 CSV_FILENAME = FILEPATH + 'train_ship_segmentations.csv'
 DATA_FILEPATH = FILEPATH + 'train/'
-TRAIN_INPUT_SAVE = FILEPATH + 'train_images_6'
-TRAIN_LABEL_SAVE = FILEPATH + 'train_labels_6'
+TRAIN_INPUT_SAVE = FILEPATH + 'train_images'
+TRAIN_LABEL_COARSE_SAVE = FILEPATH + 'train_images_coarse'
 PERM_MODEL_FILEPATH = '/Models/Ships/model.ckpt' #filepaths to model and summaries
 SUMMARY_FILEPATH ='/Models/Ships/Summaries/'
 
-RESTORE = False
-WHEN_DISP = 100
+RESTORE = True
+WHEN_DISP = 10
 WHEN_SAVE = 2000
-MAX_OUTPUTS = 32
+MAX_OUTPUTS = 16
 ITERATIONS = 1000000
 LEARNING_RATE = 3e-3
 
-CONVOLUTIONS = [32, 64, 128,256]
-DIVIDEND = 6
+CONVOLUTIONS = [32, 64, 128, 256]
+DIVIDEND = 12
 BASE_INPUT_SHAPE = 768, 768, 3
 BASE_OUTPUT_SHAPE = 768, 768, 1
+COURSE_SHAPE = DIVIDEND, DIVIDEND
 INPUT_SHAPE = BASE_INPUT_SHAPE[0]/DIVIDEND, BASE_INPUT_SHAPE[1]/DIVIDEND, 3
 OUTPUT_SHAPE = BASE_OUTPUT_SHAPE[0]/DIVIDEND, BASE_OUTPUT_SHAPE[1]/DIVIDEND, 1
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 
 def decode_image(image):
     # Normalize from [0, 255] to [0.0, 1.0]
     image = tf.decode_raw(image, tf.uint8)
     image = tf.cast(image, tf.float32)
-    image = tf.reshape(image, [INPUT_SHAPE[0], INPUT_SHAPE[1], 3])
+    image = tf.reshape(image, [BASE_INPUT_SHAPE[0], BASE_INPUT_SHAPE[1], 3])
     return image / 255.0
+
+def decode_label_coarse(label):
+    label = tf.decode_raw(label, tf.uint8)
+    label = tf.reshape(label, [COURSE_SHAPE[0], COURSE_SHAPE[1]])
+    label = tf.cast(label, tf.float32)
+    return label
 
 def decode_label(label):
     label = tf.decode_raw(label, tf.uint8)
-    label = tf.reshape(label, [OUTPUT_SHAPE[0], OUTPUT_SHAPE[1]])
+    label = tf.reshape(label, [COURSE_SHAPE[0], COURSE_SHAPE[1]])
     label = tf.cast(label, tf.float32)
     condition = tf.equal(label, 0)
     case_true = tf.zeros_like(label)+ .1
@@ -47,45 +54,70 @@ def decode_label(label):
 
 def return_datatset_train():
     images = tf.data.FixedLengthRecordDataset(
-      TRAIN_INPUT_SAVE, INPUT_SHAPE[0] * INPUT_SHAPE[1] * 3).map(decode_image)
+      TRAIN_INPUT_SAVE, BASE_INPUT_SHAPE[0] * BASE_INPUT_SHAPE[1] * 3).map(decode_image)
     labels = tf.data.FixedLengthRecordDataset(
-      TRAIN_LABEL_SAVE, OUTPUT_SHAPE[0]* OUTPUT_SHAPE[1]).map(decode_label)
+      TRAIN_LABEL_COARSE_SAVE, COURSE_SHAPE[0]* COURSE_SHAPE[1]).map(decode_label_coarse)
     return tf.data.Dataset.zip((images, labels))
 
 def build_model(x, labels):
     conv_pointers = [InputLayer(x, name= 'disc_inputs')]
     for i,v in enumerate(CONVOLUTIONS):
-        curr_layer = BatchNormLayer(Conv2d(Conv2d(conv_pointers[-1],
-            CONVOLUTIONS[i], (1, 1),strides = (1,1), name=
-            '1x1_conv1_%s'%(i)),
-            CONVOLUTIONS[i], (5, 5),strides = (1,1), name=
+        curr_layer = BatchNormLayer(Conv2d(conv_pointers[-1],
+            CONVOLUTIONS[i], (5, 5),strides = (2,2), name=
             'conv1_%s'%(i)),
             act=tf.nn.leaky_relu,is_train=True ,name=
             'batch_norm%s'%(i))
 
-
-
-
         if i < len(CONVOLUTIONS)-1:
-            conv_pointers.append(ConcatLayer([curr_layer, conv_pointers[-1]],
-             3, name = 'concat_layer%s'%(i)))
+            # conv_pointers.append(ConcatLayer([curr_layer, conv_pointers[-1]],
+            #  3, name = 'concat_layer%s'%(i)))
+            conv_pointers.append(curr_layer)
         else:
             conv_pointers.append(curr_layer)
     # y_conv = DenseLayer(flat, m.fully_connected_size, act=tf.nn.relu,name =  'hidden_encode')
-    output = BatchNormLayer(Conv2d(Conv2d(conv_pointers[-1],
-        CONVOLUTIONS[i], (1, 1),strides = (1,1), name='1x1_Final_Conv'),
-        1, (5, 5),strides = (1,1), name='Final_Conv'),is_train=True ,name='Final_Norm')
-
-    logits = FlattenLayer(output).outputs
+    pre_max_pool = Conv2d(conv_pointers[-1],
+        1, (5, 5),strides = (1,1), name='Final_Conv')
+    _, pm_width, pm_height, _ = pre_max_pool.outputs.get_shape()
+    max_pool_width, max_pool_height = pm_width/DIVIDEND, pm_height/DIVIDEND
+    max_pool = MaxPool2d(pre_max_pool, filter_size = (max_pool_width, max_pool_height), strides = (max_pool_width, max_pool_height), name = 'Final_Pool')
+    logits = FlattenLayer(max_pool).outputs
     flat_labels = tf.contrib.layers.flatten(labels)
     cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=flat_labels, logits=logits))
     train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
-    tiled_labels = tf.tile(tf.expand_dims(labels, axis = -1), [1,1,1,3])
-    tiled_outputs = tf.tile(tf.sigmoid(output.outputs), [1,1,1,3])
-    input_summary = tf.summary.image("Example", tf.concat([x, tiled_labels, tiled_outputs], axis = 2),max_outputs = MAX_OUTPUTS)#show fake image
+    final_guess = tf.round(tf.sigmoid(logits))
+
+
+
+    accuracy_summary = tf.summary.scalar('Accuracy', tf.reduce_mean(tf.cast(tf.equal(flat_labels, final_guess), tf.float32)))
+    percent_found_summary_round = tf.summary.scalar('Percent Found Rounded', tf.reduce_mean(final_guess))
+    percent_found_summary = tf.summary.scalar('Percent Found Nonrounded', tf.reduce_mean(tf.sigmoid(logits)))
+
+    flat_labels = tf.cast(flat_labels, tf.float64)
+    final_guess = tf.cast(final_guess, tf.float64)
+    TP = tf.count_nonzero(final_guess * flat_labels, dtype=tf.float32)
+    TN = tf.count_nonzero((final_guess - 1) * (flat_labels - 1), dtype=tf.float32)
+    FP = tf.count_nonzero(final_guess * (flat_labels - 1), dtype=tf.float32)
+    FN = tf.count_nonzero((final_guess - 1) * flat_labels, dtype=tf.float32)
+    true_positive = tf.divide(TP, TP + FP)
+    true_negative = tf.divide(TN, TN + FN)
+    # accuracy = tf.divide(TP + TN, TN + FN + TP + FP)
+    # accuracy_summary = tf.summary.scalar('Accuracy', accuracy)
+    true_positive_summary =tf.summary.scalar('True Positive',true_positive)
+    true_negative_summary =tf.summary.scalar('True Negative',true_negative)
+    # resized_label = tf.image.resize_images(labels,size = (BASE_INPUT_SHAPE[0], BASE_INPUT_SHAPE[1]))
+    # resized_output = tf.image.resize_images(tf.sigmoid(max_pool.outputs),size = (BASE_INPUT_SHAPE[0], BASE_INPUT_SHAPE[1]))
+    # tiled_labels = tf.tile(tf.expand_dims(resized_label, axis = -1), [1,1,1,3])
+    # tiled_outputs = tf.tile(resized_output, [1,1,1,3])
+
+    image_summary = tf.summary.image("Example", tf.concat([tf.sigmoid(max_pool.outputs), tf.expand_dims(labels, axis = -1), tf.ones_like(max_pool.outputs)], axis = 2),max_outputs = MAX_OUTPUTS)#show fake image
+    # image_summary_2 = tf.summary.image("Example_2", x,max_outputs = MAX_OUTPUTS)#show fake image
+    # image_summary_merge = tf.summary.merge([image_summary,image_summary_2])
+
     cross_entropy_summary = tf.summary.scalar('Loss',cross_entropy)
-    # real_summary = tf.summary.merge([input_summary,cross_entropy_summary])
-    return input_summary, cross_entropy_summary , train_step
+    real_summary = tf.summary.merge([cross_entropy_summary,accuracy_summary,
+        percent_found_summary_round,percent_found_summary, true_negative_summary,
+        true_positive_summary])
+    return real_summary , image_summary,train_step
 
 
 
@@ -93,14 +125,14 @@ if __name__ == "__main__":
 
     sess = tf.Session()#start the session
     ##############GET DATA###############
-    train_ship = return_datatset_train().shuffle(buffer_size= 5000).repeat().batch(BATCH_SIZE)
+    train_ship = return_datatset_train().repeat().batch(BATCH_SIZE)
     # test_ship = getData.return_mnist_dataset_test().repeat().batch(TEST_BATCH_SIZE)
     train_iterator = train_ship.make_initializable_iterator()
     # test_iterator = test_mnist.make_initializable_iterator()
     train_input, train_label = train_iterator.get_next()
     # test_input = test_iterator.get_next()
     sess.run([train_iterator.initializer])
-    input_summary, cross_entropy_summary, train_step = build_model(train_input, train_label)
+    input_summary, image_summary,train_step = build_model(train_input, train_label)
 
     ##########################################################################
     #Call function to make tf models
@@ -117,11 +149,14 @@ if __name__ == "__main__":
     train_writer = tf.summary.FileWriter(SUMMARY_FILEPATH,
                                   sess.graph)
     for i in range(ITERATIONS):
-        input_summary_ex, cross_entropy_summary_ex, _= sess.run([input_summary, cross_entropy_summary, train_step])
-        train_writer.add_summary(cross_entropy_summary_ex, i)
-
         if not i % WHEN_DISP:
+            input_summary_ex, image_summary_ex, _= sess.run([input_summary,image_summary ,train_step])
+            train_writer.add_summary(image_summary_ex, i)
             train_writer.add_summary(input_summary_ex, i)
+        else:
+            input_summary_ex, _= sess.run([input_summary, train_step])
+            train_writer.add_summary(input_summary_ex, i)
+
         if not i % WHEN_SAVE:
             saver_perm.save(sess, PERM_MODEL_FILEPATH)
 
